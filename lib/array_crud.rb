2bad14c195a0614c179ef3c4655b64fad09422fb
Edgar Rivera <edgar.a.rivera08@gmail.com>
2020-02-01 20:07:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,8 +27,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array.shift
-  p array.shift
+  new_array = array.shift
+  p new_array
 end
 
 def retrieve_last_element_from_array(array)
